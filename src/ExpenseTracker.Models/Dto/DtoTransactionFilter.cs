b21e275de5727619615b848bec080401fe5f46e4
Akshay Tilekar <akshaytilekar7@gmail.com>
2024-09-10 17:58:47 +0530

    asdasda

--- src/ExpenseTracker.Models/Dto/DtoTransactionFilter.cs
+++ src/ExpenseTracker.Models/Dto/DtoTransactionFilter.cs
@@ -1,15 +1,27 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 namespace ExpenseTracker.Models.Dto
 {
     public class DtoTransactionFilter
     {
+        [Display(Name = "Start Date")]
         public DateTime StartDate { get; set; }
+
+        [Display(Name = "End Date")]
         public DateTime EndDate { get; set; }
         public decimal? Amount { get; set; }
+
+        [Display(Name = "Sub category")]
         public int SubCategoryId { get; set; }
+        
+        [Display(Name = "Category")]
         public int CategoryId { get; set; }
         public string Comment { get; set; }
+
+        [Display(Name = "User name")]
         public int UserId { get; set; }
+        
+        [Display(Name = "Bank Name")]
         public int BankId { get; set; }
 
     }
--- src/ExpenseTracker.Models/Income.cs
+++ src/ExpenseTracker.Models/Income.cs
@@ -1,5 +1,6 @@
 ﻿using ExpenseTracker.Models.Common;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ExpenseTracker.Models
 {
@@ -9,8 +10,12 @@ namespace ExpenseTracker.Models
         public DateTime Date { get; set; }
         public string Comment { get; set; }
         public User User { get; set; }
+
+        [Display(Name = "User Name")]
         public int UserId { get; set; }
         public Bank Bank { get; set; }
+
+        [Display(Name = "Bank Name")]
         public int BankId { get; set; }
 
     }
